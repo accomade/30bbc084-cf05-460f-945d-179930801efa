674fb5d31cf6b55a6f09c71b5764e8d09685c0d7
Dirk Porsche <dirkporsche78@googlemail.com>
2023-10-29 15:16:07 +0100

    Initial commit

---
+++ src/lib/components/blocks/Text.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { currentLang } from '$lib/stores/lang';
+  import { i18n } from '$lib/conf';
+ 
+  export let ref:string
+  
+  $: {
+    console.log('Text lang', $currentLang)
+    console.log('trans', i18n.translations[$currentLang].calendar.weekendLabel)
+  }
+
+  $: trans = i18n.translations[$currentLang];
+  $: out = trans.site[ref] ?? ref
+  
+  const innerHtml = (node:HTMLDivElement) => {
+    node.innerHTML = out;
+    return {
+      update: (html:string) => {
+        node.innerHTML = out
+      }
+    }
+  }
+</script>
+
+<div class="content" use:innerHtml>
+
+</div>
+
+<style>
+  .content {
+    font-size: clamp(1rem, 2cqh, 2rem);
+    
+    text-align: left;
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+  }
+</style>
